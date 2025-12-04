07f05f6ceef3d2aad863eef395d85748386b5ed4
Anant Goyal <anantgoyal2000@gmail.com>
2025-12-04 17:17:22 -0600

    updating java class path

--- src/main/java/com/uiuc/systems/WorkerTaskServer.java
+++ src/main/java/com/uiuc/systems/WorkerTaskServer.java
@@ -56,7 +56,7 @@ public class WorkerTaskServer implements Runnable{
                 List<String> cmd = new ArrayList<>();
                 cmd.add("java");
                 cmd.add("-cp");
-                cmd.add("target/HybridDistributedFileSystem-1.0-SNAPSHOT.jar");
+                cmd.add("/home/anantg2/mp4-g76/target/HybridDistributedFileSystem-1.0-SNAPSHOT.jar");
                 cmd.add("com.uiuc.systems.WorkerTask");
                 cmd.add(((StartWorkerTaskRequest) obj).getLeaderIp());
                 Integer port = ((StartWorkerTaskRequest) obj).getLeaderPort();
@@ -68,7 +68,12 @@ public class WorkerTaskServer implements Runnable{
                 cmd.add(((StartWorkerTaskRequest) obj).getOutputFileName());
                 cmd.addAll(((StartWorkerTaskRequest) obj).getOperatorArgs());
 
+                System.out.println("Starting WorkerTask with command: " + String.join(" ", cmd));
+
                 Process p = new ProcessBuilder(cmd).redirectErrorStream(true).start();
+
+                System.out.println("process started with PID=" + p.pid());
+
                 int taskId = ((StartWorkerTaskRequest) obj).getTaskId();
                 taskProcessMap.put(taskId, p);
                 System.out.println("Started WorkerTask: " + ((StartWorkerTaskRequest) obj).getTaskId());
